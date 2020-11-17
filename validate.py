e85c13ba24d2c5472533e1c8374d01edc9b300a0
msjones3 <51344555+msjones3@users.noreply.github.com>
2020-11-18 05:08:02 +1000

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,11 +1,11 @@
 def isEmpty(input):
-  # return true if input is blank
-  return False
+    # returns true if input is blank or only spaces
+    return False
 
 def credentialsValid(username, password):
-  # return true if username is 'admin' and password is 'secret'
-  return True
+    # return true if username is 'admin' and password is 'secret'
+    return True
 
 def numberValid(number):
-  # return true if number is an integer between 1 and 10
-  return True
+    # return true if number is an integer between 1 and 10
+    return True
