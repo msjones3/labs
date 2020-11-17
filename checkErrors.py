b72e8f19a10c3688f683fa7709b131d4a7a9f242
msjones3 <51344555+msjones3@users.noreply.github.com>
2020-11-18 04:53:34 +1000

    Update checkErrors.py

--- checkErrors.py
+++ checkErrors.py
@@ -9,14 +9,16 @@ myForm = {
     }
 
 def evaluateErrors(form):
+    # takes in a dictionary
+    # return a list of errors on running the tests in validate.py on given form values
     errors = []
     for item in form:
         if isEmpty(form[item]):
             errors.append(f'{item} cannot be blank')
     if not numberValid(form['number']):
         errors.append('number must be between 1 and 10. If you would like to purchase more tickets, please ring us.')
+    # to do: add the credentialsValid check here!
     return(errors)
 
 errors = evaluateErrors(myForm)
-for error in errors:
-    print(error)
+
